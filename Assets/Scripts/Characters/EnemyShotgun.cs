2523fa65db901bb81c44dec20d245edbe7205038
William Livingston <71736903+WilliamHenryLivingston@users.noreply.github.com>
2024-11-18 13:51:47 -0800

    Added repo
    Created new enemy class

---
+++ Assets/Scripts/Characters/EnemyShotgun.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotgun : Enemy
+{
+
+    public override void Move(Vector2 direction)
+    {
+        base.Move(direction);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+    }
+}
